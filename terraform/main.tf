a3f8819deaab3a5744e5877d93cf6f74fe2e8d57
Aslamdevops <aslamdevops@outlook.com>
2025-08-27 20:04:10 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,14 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-0a232144cf20a27a5"   # Amazon Linux 2 AMI (update with latest)
   instance_type = "t2.micro"
+  key_name      = var.key_name
   tags = { Name = "c8.local" }
 }
 
 resource "aws_instance" "backend" {
   ami           = "ami-0bbdd8c17ed981ef9"   # Ubuntu 21.04 AMI (update with latest)
   instance_type = "t2.micro"
+  key_name      = var.key_name
   tags = { Name = "u21.local" }
 }
 
